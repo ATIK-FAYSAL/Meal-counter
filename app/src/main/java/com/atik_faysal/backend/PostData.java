7eb0b39a53165355d5c963f12f9e305625d6ed04
ATIK_FAYSAL <atikfaysal1404@gmail.com>
2018-11-12 01:30:53 +0600

    update

--- app/src/main/java/com/atik_faysal/backend/PostData.java
+++ app/src/main/java/com/atik_faysal/backend/PostData.java
@@ -1,26 +1,5 @@
 package com.atik_faysal.backend;
 
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import android.content.Context;
@@ -65,9 +44,7 @@ public class PostData
                     },
                     new Response.ErrorListener() {
                          @Override
-                         public void onErrorResponse(VolleyError volleyError) {
-
-                         }
+                         public void onErrorResponse(VolleyError volleyError) {}
                     }) {
                     @Override
                     protected Map<String, String> getParams() {
